2281cfca747a2003f6ebd0de769007f09bf61f29
Raymond Cheung <darkray16@gmail.com>
2016-06-14 13:23:34 -0700

    Generate pairings now creates score groups, and ranks each group according to tiebreakers. Still needs unit testing to check that sorting is error-proof.

--- public/scripts/controllers/controlArea.js
+++ public/scripts/controllers/controlArea.js
@@ -1,11 +1,11 @@
 angular.module('tournamentOrganizerApp')
-  .controller('controlAreaController', ['$scope', '$location', 'sessionStore',
-    function($scope, $location, sessionStore) {
+  .controller('controlAreaController', ['$scope', '$location', 'sessionStore', 'generatePairings',
+    function($scope, $location, sessionStore, generatePairings) {
       var currentRound = 2;
 
       var formdata = {
         timer: 45,
-        format: "Round Robin",
+        format: "swiss",
         players: {
           "1001": "John",
           "2020": "Brian",
@@ -16,39 +16,74 @@ angular.module('tournamentOrganizerApp')
           "1000": "Chris",
           "1100": "Ray"
         },
-        matches: {
-          rounds: {
-            "1": [
-              {
-                players: ["1001", "2020"],
-                result: {
-                  "1001": 2,
-                  "2020": 1
-                }
-              },
-              {
-                players: ["3300", "4890"],
-                result: {
-                  "3300": 0,
-                  "4890": 2
-                }
-              },
-              {
-                players: ["1111", "0001"],
-                result: {
-                  "1111": 1,
-                  "0001": 1
-                }
-              },
-              {
-                players: ["1000", "1100"],
-                result: {
-                  "1000": 0,
-                  "1100": 1
-                }
+        config: {
+          winScore: 3,
+          loseScore: 0,
+          drawScore: 1
+        },
+        rounds: {
+          "1": [
+            {
+              players: ["1001", "2020"],
+              result: {
+                "1001": 2,
+                "2020": 1
               }
-            ]
-          }
+            },
+            {
+              players: ["3300", "4890"],
+              result: {
+                "3300": 0,
+                "4890": 1,
+                "draws": 1
+              }
+            },
+            {
+              players: ["1111", "0001"],
+              result: {
+                "1111": 2,
+                "0001": 1
+              }
+            },
+            {
+              players: ["1000", "1100"],
+              result: {
+                "1000": 1,
+                "1100": 1,
+                "draws": 1
+              }
+            }
+          ],
+          "2": [
+            {
+              players: ["0001", "2020"],
+              result: {
+                "0001": 2,
+                "2020": 1
+              }
+            },
+            {
+              players: ["3300", "1000"],
+              result: {
+                "3300": 0,
+                "1000": 2
+              }
+            },
+            {
+              players: ["1100", "4890"],
+              result: {
+                "1100": 0,
+                "4890": 2
+              }
+            },
+            {
+              players: ["1111", "1001"],
+              result: {
+                "1111": 2,
+                "1001": 1
+              }
+            }
+          ]
         }
       }
       $scope.lockRows = {};
@@ -61,7 +96,7 @@ angular.module('tournamentOrganizerApp')
             ++totalLocked;
           }
         }
-        $scope.progress = totalLocked / $scope.matches.rounds[$scope.currentRound-1].length * 100;
+        $scope.progress = totalLocked / $scope.rounds[$scope.currentRound].length * 100;
       };
       $scope.highlightWinner = function(row, side) {
         var opp = side === "right" ? "left" : "right";
@@ -75,12 +110,16 @@ angular.module('tournamentOrganizerApp')
       $scope.timer = formdata.timer;
       $scope.format = formdata.format;
       $scope.playerNames = formdata.players;
-      $scope.matches = formdata.matches;
-      $scope.currentRound = ($location.search().currentRound || $scope.matches.rounds.length);
-      for(match in formdata.matches.rounds[$scope.currentRound-1]) {
+      $scope.rounds = formdata.rounds;
+      $scope.currentRound = ($location.search().currentRound || Object.keys($scope.rounds).length);
+      for(match in formdata.rounds[$scope.currentRound]) {
         $scope.matchScore[match] = {
           "left": null,
           "right": null
         }
+      }
+      
+      $scope.finishRound = function() {
+        generatePairings.generatePairings(formdata);
       }
 }]);
--- public/scripts/services/generatePairings.js
+++ public/scripts/services/generatePairings.js
@@ -1,99 +1,253 @@
 angular.module('tournamentOrganizerApp')
   .factory('generatePairings', function() {
-    return function(eventInfo) {
-      var pairingsGenerator = {
-        generateRandomPairings: function(playerList) {
-          var removeValueFromArray = function(value, arr) {
-            for(var i = arr.length - 1; i >= 0; i--) {
-              if(arr[i] === value) {
-                arr.splice(i, 1);
+    return {
+      generatePairings: function(tournamentDocuments) {
+        var populatePlayerInfo = function(tournamentDocument) {
+          var addResultToPlayersInfo = function(result, playersInfo) {
+            var isMatchResultDraw = function(result) {
+              var first;
+              for (var index in result) {
+                if (index !== "draw") {
+                  if (first === undefined) {
+                    first = result[index];
+                  } else {
+                    if (first !== result[index]) {
+                      return false;
+                    }
+                  }
+                }
               }
+              return true;
+            };
+            var getWinnerOfMatch = function(result) {
+              var highestScore,
+                  highestPlayer;
+              for(var i in result) {
+                if (i !== "draws") {
+                  if ( typeof highestScore === "undefined" || highestScore < result[i]) {
+                    highestScore = result[i];
+                    highestPlayer = i;
+                  }
+                }
+              }
+              return highestPlayer;
             }
-          };
-          var seating;
-          for(var player in playerList) {
-            playerA = playerList[Math.floor(Math.random()*playerList.length)];
-            removeValueFromArray(playerA, playerList);
-            playerB = playerList[Math.floor(Math.random()*playerList.length)];
-            removeValueFromArray(playerB, playerList);
-            pairings[seating] = [playerA, playerB];
-          }
-        },
 
-        generateNewPairings: function() {
-
-          var modifyScores = function(playerScores, winner, loser, draw) {
-            var winScore = this.tournament.config.winScore,
-                loseScore = this.tournament.config.loseScore,
-                drawScore = this.tournament.config.drawScore;
-            draw = draw || false;
-            if (draw) {
-              playerScores[winner] += drawScore;
-              playerScores[loser] += drawScore;
-            } else {
-              playerScores[winner] += winScore;
-              playerScores[loser] += loseScore;
-            }
-          };
-
-          if (!this.format) {
-            throw "this.format not set for PairingsGenerator.  Required before calling generateNewPairings().";
-          }
-          if (!this.tournament) {
-            throw "this.tournament not set for PairingsGenerator.  Required before calling generateNewPairings().";
-          }
-          var pairings,
-              playerScores = {},
-              playerHistories = {};
-          for (var playerId in  this.tournament.players) {
-            playerScores[playerId] = 0;
-            playerHistories[playerId] = [];
-          }
-
-          if (this.format === "swiss") {
-            if (!("1" in this.tournament.matches.rounds)) {
-              //this is first round
-              var playerList = this.tournament.players.keys().sort();
-              pairings = this.generateRandomPairings(playersList);
-            }
-            
-            for(match in this.tournament.matches.rounds) {
-              playersInMatch = this.tournament.matches.rounds[match].keys();
-              playerAScore = this.tournament.matches.rounds[match][playersInMatch[0]];
-              playerBScore = this.tournament.matches.rounds[match][playersInMatch[1]];
-              if (playerAScore > playerBScore) {
-                modifyScores(playerScores, playerInMatch[0], playerInMatch[1]);
-              } else if (playerBScore > playerAScore) {
-                modifyScores(playerScores, playerInMatch[1], playerInMatch[0]);
+            var playerIds = Object.keys(result),
+                playerId,
+                playerInMatch,
+                opponentInMatch;
+            //First we add all game wins/losses/draws to player records(for tiebreaker purposes).
+            for(var i in result) {
+              if (i !== "draws") {
+                for(var j in playerIds) {
+                  playerId = playerIds[j];
+                  if (playerId !== "draws") {
+                    if (playerId !== i) {
+                      playersInfo[playerId].game.loss += result[i];
+                    } else {
+                      playersInfo[playerId].game.win += result[i];
+                    }
+                  }
+                }
               } else {
-                //draw
-                modifyScores(playerScores, playerInMatch[0], playerInMatch[1], true);
+                for(var j in playerIds) {
+                  if (playerId !== "draws") {
+                    playerId = playerIds[j];
+                    playersInfo[playerId].game.draw += result[i];
+                  }
+                }
               }
-              playerHistories[playersInMatch[0]].push(playersInMatch[1]);
-              playerHistories[playersInMatch[1]].push(playersInMatch[0]);
             }
-          } else if (this.format === "double elimination") {
 
-          } else if (this.format === "single elimination") {
+            //Now, we increment match wins/losses/draws...
+            if (isMatchResultDraw(result)) {
+              for(var z in playerIds) {
+                playerInMatch = playerIds[z];
+                if (playerInMatch !== "draws") {
+                  playersInfo[playerInMatch].match.draw++;
+                }
+              }
+            } else {
+              var winner = getWinnerOfMatch(result);
+              playersInfo[winner].match.win++;
+              for (var h in playerIds) {
+                var loser = playerIds[h];
+                if (loser !== "draws") {
+                  if (loser !== winner) {
+                    playersInfo[loser].match.loss++;
+                  }
+                }
+              }
+            }
+            // We add record of all opponents for given player
+            for(var k in playerIds) {
+              playerInMatch = playerIds[k];
+              if (playerInMatch !== "draws") {
+                for (var l in playerIds) {
+                  opponentInMatch = playerIds[l];
+                  if (opponentInMatch !== "draws" && opponentInMatch !== playerInMatch) {
+                    playersInfo[playerInMatch].opponents.push(opponentInMatch);
+                  }
+                } // loop through for opponents
+              }
+            } // loop through for player
+          };
 
-          } else if (this.format === "round robin") {
-
+          var winScore = tournamentDocument.config.winScore,
+              loseScore = tournamentDocument.config.loseScore,
+              drawScore = tournamentDocument.config.drawScore,
+              totalRounds = tournamentDocument.rounds.length,
+              playersInfo = {};
+          // we set an empty entry for each player
+          for (var playerId in tournamentDocument.players) {
+            playersInfo[playerId] = {
+              'match': {
+                'win': 0,
+                'loss': 0,
+                'draw': 0,
+                'getMatchPoints': function() {
+                  var winPts = this.win * winScore,
+                      lossPts = this.loss * loseScore,
+                      drawPts = this.draw * drawScore;
+                  return winPts + lossPts + drawPts;
+                },
+                'getWinBreakers': function() {
+                  var maxPts = (this.win + this.loss + this.draw) * winScore;
+                  return this.getMatchPoints() / maxPts;
+                }
+              },
+              'game': {
+                'win': 0,
+                'loss': 0,
+                'draw': 0,
+                'getGamePoints': function() {
+                  var winPts = this.win * winScore,
+                      lossPts = this.loss * loseScore,
+                      drawPts = this.draw * drawScore;
+                  return winPts + lossPts + drawPts;
+                },
+                'getWinBreakers': function() {
+                  var maxPts = (this.win + this.loss + this.draw) * winScore;
+                  return this.getGamePoints() / maxPts;
+                }
+              },
+              'opponents': []
+            };
           }
 
-        },
-        setTournamentObj: function(tournament) {
-          this.tournament = tournament;
-        },
-        setTournamentType: function(type) {
-          var types = [
-            "swiss", "double elimination", "single elimination", "round robin"
-          ];
-          if (types.indexOf(type) < 0) {
-            throw "Invalid tournament type: " + type + " submitted to pairingsGenerator object.";
+          // We iterate through each round and match and start adding information
+          for (var round in tournamentDocument.rounds) {
+            for (var match in tournamentDocument.rounds[round]) {
+              addResultToPlayersInfo(tournamentDocument.rounds[round][match].result, playersInfo);
+            } // loop through each match
+          } // loop through each round
+          return playersInfo;
+        };
+
+        var populateScoreGroups = function(playersInfo) {
+          var scoreGroups = {},
+              score;
+          for(var playerId in playersInfo) {
+            score = playersInfo[playerId].match.getMatchPoints();
+            scoreGroups[score] = scoreGroups[score] ? scoreGroups[score] : [];
+            scoreGroups[score].push(playerId);
           }
-          this.format = type;
+          var sortEachScoreGroup = function(playerScores) {
+            var hasBetterTiebeakers = function(player, opponent, playersInfo) {
+              var tiebreakers = ["opponent.match", "game", "opponent.game"],
+                  playerTieBreakers,
+                  opponentTieBreakers;
+              for(var index in tiebreakers) {
+                var tiebreaker = tiebreakers[index];
+                if (tiebreaker.indexOf("opponent.") === 0) {
+                  var oppBreaker = tiebreaker.slice(9).split("."),
+                      tally = 0,
+                      count = 0;
+                  for (var x in player.opponents) {
+                    tally += playersInfo[player.opponents[x]][oppBreaker[0]].getWinBreakers();
+                    count++
+                  }
+                  playerTieBreakers = tally / count;
+                  tally = 0,
+                      count = 0;
+                  for (var x in opponent.opponents) {
+                    tally += playersInfo[opponent.opponents[x]][oppBreaker[0]].getWinBreakers();
+                    count++
+                  }
+                  opponentTieBreakers = tally / count;
+
+                } else {
+                  playerTieBreakers = player.game.getWinBreakers(),
+                      opponentTieBreakers = opponent.game.getWinBreakers();
+                }
+                if (playerTieBreakers > opponentTieBreakers) {
+                  return true;
+                } else if (opponentTieBreakers > playerTieBreakers) {
+                  return false;
+                } else if (playerTieBreakers === opponentTieBreakers) {
+                  continue;
+                } else {
+                  throw "Could not calculate tiebreakers...";
+                }
+              }
+            };
+            //let's quicksort
+            var quicksort = function(arr) {
+              if (arr.length === 0) {
+                return [];
+              }
+              var left = [];
+              var right = [];
+              var pivot = arr[0];
+
+              for(var i=1;i<arr.length;i++) {
+                if (hasBetterTiebeakers(playersInfo[arr[i]], playersInfo[pivot], playersInfo)) {
+                  left.push(arr[i]);
+                }else {
+                  right.push(arr[i]);
+                }
+              }
+              return quicksort(left).concat(pivot, quicksort(right));
+            };
+
+            for(var scoreGroup in playerScores) {
+              playerScores[scoreGroup] = quicksort(playerScores[scoreGroup]);
+            }
+          };
+          sortEachScoreGroup(scoreGroups);
+          return scoreGroups;
+        };
+
+        var matchPlayers = function(playerScoreGroups) {
+          
+          
+        };
+
+        var playerInfo = populatePlayerInfo(tournamentDocuments),
+            scoreGroups = populateScoreGroups(playerInfo),
+            pairings = matchPlayers(scoreGroups);
+
+
+        return pairings;
+      },
+
+      generateRandomPairings: function (playerList) {
+        var removeValueFromArray = function (value, arr) {
+          for (var i = arr.length - 1; i >= 0; i--) {
+            if (arr[i] === value) {
+              arr.splice(i, 1);
+            }
+          }
+        };
+        var seating;
+        for (var player in playerList) {
+          playerA = playerList[Math.floor(Math.random() * playerList.length)];
+          removeValueFromArray(playerA, playerList);
+          playerB = playerList[Math.floor(Math.random() * playerList.length)];
+          removeValueFromArray(playerB, playerList);
+          pairings[seating] = [playerA, playerB];
         }
-
-      }
+      },
     };
   });
